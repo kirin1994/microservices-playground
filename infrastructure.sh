d247bb3bfaad2261444b9ac4f5d6f28bfffa9036
Adrian <apodlawski@gmail.com>
2020-03-22 17:38:27 +0100

    Infrastructure in vagrant

---
+++ infrastructure.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+VAGRANT_PATH="k8s-cluster"
+
+case $1 in
+    "up") cd $VAGRANT_PATH && vagrant up ;;
+    "destroy") cd $VAGRANT_PATH && vagrant destroy ;;
+    *) echo "Param not found"
+esac
