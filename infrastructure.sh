3435fde9fb7fd0debe2ca2f1a1bfe06f0cdd59b8
Adrian <apodlawski@gmail.com>
2020-03-23 23:42:02 +0100

    ansible inventory update and k8s plabook init

--- infrastructure.sh
+++ infrastructure.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 VAGRANT_PATH="k8s-cluster/infrastructure"
+ANSIBLE_PATH="k8s-cluster/infrastructure/ansible"
 
 case $1 in
     "up") cd $VAGRANT_PATH && vagrant up ;;
     "destroy") cd $VAGRANT_PATH && vagrant destroy ;;
+    "ping") ansible -i $ANSIBLE_INVENTORY_PATH/inventory all -m ping ;;
+    "k8s-install") ansible-playbook -i k8s-cluster/infrastructure/ansible/inventory  k8s-cluster/infrastructure/ansible/k8s-install-playbook.yaml ;;
     *) echo "Param not found"
 esac
